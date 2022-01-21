7d3ec53965a9bc9a839fd4354540459ee89a1e58
ERDLesage <elise.r.d.lesage@gmail.com>
2022-01-21 16:19:18 +0100

    tidy data; remove too-fast RTs

--- LGthesis_wrapper.R
+++ LGthesis_wrapper.R
@@ -3,27 +3,30 @@
 library(tidyverse)
 
 
-
 # load in data ####
 setwd("C:/Users/elise.000/Documents/AAA_projects/Lisa_data/")
-D <- readxl::read_xlsx('Gorilla_exp_V23_2.2_Lisa.xlsx')
+D_ <- readxl::read_xlsx('Gorilla_exp_V23_2.2_Lisa.xlsx')
 
 # tidy the dataset ####
 # puzzle with the data to figure out what the optimal choice is
 # and what the rewarded choice is.
-D$Block <- D$Block_Volgorde
-blocks <- D %>% select(Subject, Day, Block) %>% unique()
+D_$Block <- D_$Block_Volgorde
+D_ %>% select(-Incorrect, -Gerandomiseerde_Block_Volgorde, -Optellende_Block_Volgorde)
+blocks <- D_ %>% select(Subject, Day, Block) %>% unique()
+
 
 # run through blocks to add more useful variables
-for (i in 1:length(blocks)){
-  b <- D %>% filter((Subject == blocks[[i,1]] & Day == blocks[[i,2]] & Block == blocks[[i, 3]]))
+tic()
+D <-tibble()
+for (i in 1:nrow(blocks)){
+  b <- D_ %>% filter((Subject == blocks[[i,1]] & Day == blocks[[i,2]] & Block == blocks[[i, 3]]))
   b$ResponseID <- NA
   b$AnswerID <- NA
   b$OptimalChoice <- NA
-  b$ResponseID[(b$Response == "Left shovel")] <- b$LeftShovel[(b$Response == "Left shovel")]
-  b$ResponseID[(b$Response == "Left dynamite")] <- b$LeftDynamite[(b$Response == "Left dynamite")]
-  b$ResponseID[(b$Response == "Right shovel")] <- b$RightShovel[(b$Response == "Right shovel")]
-  b$ResponseID[(b$Response == "Right dynamite")] <- b$RightDynamite[(b$Response == "Right dynamite")]
+  b$ResponseID[(b$Response == "Left shovel")& !is.na(b$Response)] <- b$LeftShovel[(b$Response == "Left shovel")& !is.na(b$Response)]
+  b$ResponseID[(b$Response == "Left dynamite")& !is.na(b$Response)] <- b$LeftDynamite[(b$Response == "Left dynamite")& !is.na(b$Response)]
+  b$ResponseID[(b$Response == "Right shovel")& !is.na(b$Response)] <- b$RightShovel[(b$Response == "Right shovel")& !is.na(b$Response)]
+  b$ResponseID[(b$Response == "Right dynamite"& !is.na(b$Response))] <- b$RightDynamite[(b$Response == "Right dynamite")& !is.na(b$Response)]
   
   b$AnswerID[(b$ANSWER == "Left shovel")] <- b$LeftShovel[(b$ANSWER == "Left shovel")]
   b$AnswerID[(b$ANSWER == "Left dynamite")] <- b$LeftDynamite[(b$ANSWER == "Left dynamite")]
@@ -35,9 +38,25 @@ for (i in 1:length(blocks)){
   names <- unique(b$AnswerID)
   a <- names[order(unique(b$AnswerID))]
   FreqCorrect <- tibble(OptiResp, a)
-  rm(a, OptiResp, names)
+
   # make a column that is 8 if the answer was not optimal, and 36 if it was
   b <- inner_join(b, FreqCorrect, by = c("ResponseID" = "a"))
   b$OptimalChoice <- ifelse(b$OptiResp > 20, 1, 0)
   
+  # add to the big D :p
+  D <- rbind(D, b)
+  print(sprintf("Block %d of %d", i, nrow(blocks)))
+  rm(a, b, OptiResp, FreqCorrect, names)
 }
+rm(D_)
+toc()
+
+# clean: exclude RTs<150ms ####
+D$OptimalChoice[D$ReactionTime<150] <- NA
+D$Response[D$ReactionTime<150] <- NA
+D$ResponseID[D$ReactionTime<150] <- NA
+D$Correct[D$ReactionTime<150] <- NA
+D$Reactiontime[D$ReactionTime<150] <- NA
+
+# select: "too-early" responses; prop per block/person ####
+
