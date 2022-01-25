bd493ef3c585dda75c5af44e5846b073bbdb436f
ERDLesage <elise.r.d.lesage@gmail.com>
2022-01-25 16:42:50 +0100

    renamed themes to themes and schemes because more accuracte and cuter name

--- ERDLThemesNScchemes.R
+++ ERDLThemesNScchemes.R
@@ -1,4 +1,4 @@
-# Elise's ggplot themes 
+# Elise's ggplot themes and colour schemes
 # elise.r.d.lesage@gmail.com
 # Customised ggplot2 themes, color schemes, etc
 
@@ -33,3 +33,33 @@ theme_ERDL_simple <- function() {
   )
 }
 
+theme_ERDL_simple_nogrid <- function() {
+  theme(
+    #base_size = 20,
+    # add border 1)
+    panel.border = element_rect(colour = "Black", fill = NA, linetype = 1),
+    # color background 2)
+    panel.background = element_rect(fill = "white"),
+    # modify grid 3)
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y =  element_blank(),
+    panel.grid.minor.y = element_blank(),
+    # modify text, axis and colour 4) and 5)
+    axis.text = element_text(colour = "Black"), #, family = "TT Arial", face = "italic"
+    axis.title = element_text(colour = "Black"), #, family = "TT Arial"
+    axis.ticks = element_line(size=1,colour="Black"),
+    
+    # axis.text.x=element_text(colour="Black"), 
+    # axis.title.x = element_text(colour="Black"),
+    # axis.text.y= element_text(colour="Black"), 
+    # axis.title.y= element_text(colour="Black"),
+    # axis.line.y = element_line(colour="Black"),
+    # axis.line.x = element_line(colour="Black"),
+    # axis.ticks.y = element_line(size=1,colour="Black"),
+    # axis.ticks.x = element_line(size=1,colour="Black"),
+    
+    # legend at the bottom 6)
+    legend.position = "bottom"
+  )
+}
