4f06989a4188f8528b012750fbab0bf299c01c3f
ERDLesage <elise.r.d.lesage@gmail.com>
2022-02-07 17:27:39 +0100

    A whole lot more time spent exploring, we are still not there yet...

--- LGthesis_wrapper.R
+++ LGthesis_wrapper.R
@@ -16,19 +16,26 @@ D_ <- readxl::read_xlsx('C:/Users/elise.000/Documents/AAA_projects/Lisa_data/Gor
 # and what the rewarded choice is.
 D_ <- dplyr::rename(D_, Block = Block_Volgorde)
 D_ <- dplyr::rename(D_, RT = ReactionTime)
-D_<- D_ %>% select(-Incorrect, -Gerandomiseerde_Block_Vogorde, -Optellende_Block_Volgorde)
+#D_<- D_ %>% select(-Incorrect, -Gerandomiseerde_Block_Vogorde, -Optellende_Block_Volgorde)
 blocks <- D_ %>% select(Subject, Day, Block, StabilityContext) %>% unique()
 
 
 # run through blocks to add more useful variables
 D <-tibble()
 tic()
+nBlockRedFlags <- 0
 #for (s in 1:length(unique(D_$Subject)))
 for (i in 1:nrow(blocks)){
   b <- D_ %>% filter((Subject == blocks[[i,1]] & Day == blocks[[i,2]] & Block == blocks[[i, 3]]))
   b$ResponseID <- NA
   b$AnswerID <- NA
   b$OptimalChoice <- NA
+  b$BlockRedFlag <- NA
+  b$BlockRedFlag <-ifelse(length(unique(b$Gerandomiseerde_Block_Vogorde))>1, length(unique(b$Gerandomiseerde_Block_Vogorde)), 0)
+  if (length(unique(b$Gerandomiseerde_Block_Vogorde))>1) {
+  nBlockRedFlags <- nBlockRedFlags+1
+  disp(sprintf("Already %d Blocks red flagged.", nBlockRedFlags))
+  }
   b$ResponseID[(b$Response == "Left shovel")& !is.na(b$Response)] <- b$LeftShovel[(b$Response == "Left shovel")& !is.na(b$Response)]
   b$ResponseID[(b$Response == "Left dynamite")& !is.na(b$Response)] <- b$LeftDynamite[(b$Response == "Left dynamite")& !is.na(b$Response)]
   b$ResponseID[(b$Response == "Right shovel")& !is.na(b$Response)] <- b$RightShovel[(b$Response == "Right shovel")& !is.na(b$Response)]
@@ -45,7 +52,6 @@ for (i in 1:nrow(blocks)){
   a <- names[order(unique(b$AnswerID))]
   FreqCorrect <- tibble(FreqReward, a)
  
-
   # make a column that is 8 if the answer was not optimal, and 36 if it was
   b <- inner_join(b, FreqCorrect, by = c("ResponseID" = "a"))
   b <- dplyr::rename(b, OptiResp=FreqReward)
@@ -56,19 +62,14 @@ for (i in 1:nrow(blocks)){
   b$OptimalChoice <- ifelse(b$OptiResp > 20, 1, 0)
   b$Exception <- ifelse(b$RewardedOnThisTrial < 20, 1, 0)
   
-  # this to exclude the weird 1-trial "blocks"
-  # only add to data if it's more than 5 trials
-  #if (sum(FreqReward, na.rm=TRUE)>5) {
-    # add to the big D :p
-    D <- rbind(D, b)
-  #}
-  
-  
+  D <- rbind(D, b)
   print(sprintf("Block %d of %d", i, nrow(blocks)))
   rm(a, b, FreqCorrect, FreqReward, names)
 }
-rm(D_)
-toc()
+#rm(D_)
+time <-toc()
+disp(sprintf("That took %0.1f minutes.", time/60))
+rm(time)
 
 # QA: clean up weird blocks ####
 # all the exact doubles can go
@@ -79,17 +80,33 @@ disp(sprintf("Got rid of exact replicas (doubles): %d trials, %0.2f pct of data.
 rm(pre, post)
 # get rid of very fast responses
 D <- SageExcludeSimple(D, D$RT<150)
-# the blocks with very few trials (less than 10) I think I can safely exclude
+# the blocks with very few trials (fewer than 10) I think I can safely exclude
 qa1 <- summarySE(D, measurevar = "OptimalChoice", groupvars = c("Subject", "Day", "Block","StabilityContext"))
 targets <- qa1 %>% mutate(exclude=if_else(N<10, 1, 0)) 
 D<- SageExcludeHO(D, targets, groupvars= c("Subject", "Day", "Block","StabilityContext"), remove_data = TRUE)
+rm(qa1, targets)
+################# Further filtering ...
+OptiResp <- D %>% select(Subject, Day, StabilityContext, Block, AnswerID, RewardedOnThisTrial) %>% unique()
+OptiResp$Optimal <- ifelse(OptiResp$RewardedOnThisTrial>20, 1,0)
 
-################# pfff still need to work on this I suppose...
 qa1 <- summarySE(OptiResp, measurevar = "Optimal", groupvars = c("Subject", "Day", "Block","StabilityContext"))
 plot(qa1$N)
 qa1Anomalies <- filter(qa1, N!=4) # do this with the stability as grouper and without...
+qa2 <- summarySE(OptiResp, measurevar = "Optimal", groupvars = c("Subject", "Day", "Block"))
+plot(qa2$N)
+qa2Anomalies <- filter(qa2, N!=4) # do this with the stability as grouper and without...
 
-qa3 <- summarySE(D, measurevar = "OptimalChoice", groupvars = c("Subject", "Day", "Block","StabilityContext"))
+# Make a flag called "BlockFuckup" for this data
+targets <- qa2 %>% mutate(danger=if_else(N!=4, 1, 0)) 
+D<- SageExcludeHO(D, targets, groupvars= c("Subject", "Day", "Block"), remove_data = FALSE, var_name = "BlockFuckup")
+
+DFU <- filter(D, BlockFuckup==1)
+qa3_b <- summarySE(DFU, measurevar = "OptimalChoice", groupvars = c("Subject", "Day", "Block","StabilityContext"))
+plot(qa3_b$N)
+qa3_rb <- summarySE(DFU, measurevar = "OptimalChoice", groupvars = c("Subject", "Day", "Gerandomiseerde_Block_Vogorde","StabilityContext"))
+plot(qa3_rb$N)
+
+a <- DFU %>% filter(Subject==46, Day==2) %>% arrange(TrialNumber)
 
 qa3Anomalies <- filter(qa3, N>5)
 # how many trials in a block
@@ -108,15 +125,13 @@ plot(QA$dp)
 
 # switch history per block and person ####
 # switch history will index for each block, how long it was been since the contingency flipped
-OptiResp <- D %>% select(Subject, Day, StabilityContext, Block, AnswerID, RewardedOnThisTrial) %>% unique()
-OptiResp$Optimal <- ifelse(OptiResp$RewardedOnThisTrial>20, 1,0)
 
 # separate the conditions so that we can compare like with like
 ORStable <- filter(OptiResp, StabilityContext=="StabielC")
-ORStable<- arrange(ORStable, Subject, Day, Block, AnswerID) # make sure they have the same order
+ORStable<- arrange(ORStable, Subject, Day, Block, AnswerID) %>% select(-RewardedOnThisTrial) # make sure they have the same order
 ORStableWide <- pivot_wider(ORStable, names_from = "AnswerID", values_from = "Optimal")
 ORVolatile <- filter(OptiResp, StabilityContext=="OnstabielC")
-ORVolatile<- arrange(ORVolatile, Subject, Day, Block, AnswerID) # make sure they have the same order
+ORVolatile<- arrange(ORVolatile, Subject, Day, Block, AnswerID) %>% select(-RewardedOnThisTrial) # make sure they have the same order
 ORVolatileWide <- pivot_wider(ORVolatile, names_from = "AnswerID", values_from = "Optimal")
 
 # initialise the dataframe same, with the very first 
@@ -133,6 +148,7 @@ for (i in 1:nrow(ORVolatileWide)) {
     SwitchHistoryVolatile[i, 5:12] <- ifelse(is.na(ORVolatileWide[i,5:12]), NA, ifelse(ORVolatileWide[i,5:12]==ORVolatileWide[i-1,5:12], (SwitchHistoryVolatile[i-1,5:12]+1), 0))
   }
 }
+colnames(SwitchHistoryVolatile)<- colnames(ORVolatileWide)
 for (i in 1:nrow(ORStableWide)) {
   # if it's a new person, everything is new
   if (i==1 || (ORStableWide$Subject[i]!=ORStableWide$Subject[i-1])) {
@@ -144,9 +160,46 @@ for (i in 1:nrow(ORStableWide)) {
     SwitchHistoryStable[i, 5:12] <- ifelse(is.na(ORStableWide[i,5:12]), NA, ifelse(ORStableWide[i,5:12]==ORStableWide[i-1,5:12], (SwitchHistoryStable[i-1,5:12]+1), 0))
   }
 }
+colnames(SwitchHistoryStable)<- colnames(ORStableWide)
 
 #rm(OptiResp, ORStable, ORVolatile, ORStableWide, ORVolatileWide)
-# how many overtrained blocks
+
+# Quick and dirty using filters ####
+DF <- filter(D, BlockRedFlag==0, BlockFuckup==0)
+# General learning over time
+DF$BlockStr <- sprintf("%02d", DF$Block)
+DF<- DF %>% unite(DayBlock, Day, BlockStr, sep=".", remove = FALSE)
+DF$Subject <- as.factor(DF$Subject)
+AccDayBlock <- DF %>% group_by(Subject, DayBlock, StabilityContext) %>% summarySE(measurevar = "OptimalChoice", groupvars = c("Subject", "DayBlock", "StabilityContext"), na.rm=TRUE)
+RTDayBlock <- filter(DF, OptimalChoice==1) %>% group_by(Subject, DayBlock, StabilityContext) %>% summarySE(measurevar = "RT", groupvars = c("Subject", "DayBlock", "StabilityContext"), na.rm=TRUE)
+#acc_overtime <- summarySE(D, measurevar = "OptimalChoice", groupvars = c("Subject", "DayBlock", "StabilityContext"), na.rm=TRUE)
+
+AccuracyLines <- ggplot(AccDayBlock, aes(x=DayBlock, y=OptimalChoice, Group=Subject)) + #scale_y_continuous(limits = c(0.05, 0.2))+
+  geom_line(aes(group=Subject, colour=Subject), size = 0.1, alpha=.5) +
+  #geom_errorbar(aes(ymin=OptimalChoice-se, ymax=OptimalChoice+se, group=StabilityContext, color=Subject), width=0, size =1) +
+  #geom_point(aes(shape = Congruency, fill=Congruency, group=Congruency), stroke=1, position=position_dodge(.5), color = "Black", size = 4) +
+  facet_grid(StabilityContext~.)+
+  xlab("Overtraining") + ylab("Accuracy")+ggtitle("Accuracy over time")+
+  theme_sage_simple()
+AccuracyLines
+
+RTLines <- ggplot(RTDayBlock, aes(x=DayBlock, y=RT, Group=Subject)) + #scale_y_continuous(limits = c(0.05, 0.2))+
+  geom_line(aes(group=Subject, colour=Subject), size = 0.1, alpha=.5) +
+  #geom_errorbar(aes(ymin=OptimalChoice-se, ymax=OptimalChoice+se, group=StabilityContext, color=Subject), width=0, size =1) +
+  #geom_point(aes(shape = Congruency, fill=Congruency, group=Congruency), stroke=1, position=position_dodge(.5), color = "Black", size = 4) +
+  facet_grid(StabilityContext~.)+
+  xlab("Overtraining") + ylab("Accuracy")+ggtitle("Accuracy over time")+
+  theme_sage_simple()
+RTLines
+
+# Identify ppts who perform at chance level on day 3 ####
+AccGrp <- DF %>% group_by(Subject, Day, StabilityContext) %>% summarySE(measurevar = "OptimalChoice", groupvars = c("Subject", "Day", "StabilityContext"), na.rm=TRUE)
+QADay3 <-filter(AccGrp, Day==3)
+QADay2 <-filter(AccGrp, Day==2)
+hist(QADay3$OptimalChoice[QADay3$StabilityContext =="StabielC"])
+hist(AccDayBlock$performance)
+QADay3$Under67 <- ifelse(QADay3$performance<.67, 1, 0)
+
 
 # look at patterns re: subjects, time (condition)
 QA1 <- ggplot(QA, aes(x=dp, y=performance)) + scale_y_continuous(limits = c(0, 1))+
@@ -160,12 +213,6 @@ hist(QA$performance)
 QA$ZeroBlock <- ifelse(QA$performance<.10, 1, 0)
 ZeroBlock <- filter(QA, ZeroBlock == 1)  %>% select(Subject, Day, Block)
 
-# Identify ppts who perform at chance level on day 3 ####
-QADay3 <-filter(QAOverBlock, Day==3)
-QADay2 <-filter(QAOverBlock, Day==2)
-hist(QADay2$performance)
-hist(QADay3$performance)
-QADay3$Under67 <- ifelse(QADay3$performance<.67, 1, 0)
 
 ChanceD3 <- filter(QADay3, Under67 == 1)  %>% select(Subject)
 ChanceD3 <- unique(ChanceD3)
@@ -173,19 +220,3 @@ D$ChanceD3 <- ifelse(is.element(D$Subject, as.vector(as.matrix(ChanceD3))), 1, 0
 QA$ChanceD3 <- ifelse(is.element(QA$Subject, as.vector(as.matrix(ChanceD3))), 1, 0)
 QAOverBlock$ChanceD3 <- ifelse(is.element(QAOverBlock$Subject, as.vector(as.matrix(ChanceD3))), 1, 0)
 
-# General learning over time
-D$BlockStr <- sprintf("%02d", D$Block)
-D<- D %>% unite(DayBlock, Day, BlockStr, sep=".", remove = FALSE)
-D$Subject <- as.factor(D$Subject)
-AccDayBlock <- D %>% group_by(Subject, DayBlock, StabilityContext) %>% summarySE(measurevar = "OptimalChoice", groupvars = c("Subject", "DayBlock", "StabilityContext"), na.rm=TRUE)
-RTDayBlock <- filter(D, OptimalChoice==1) %>% group_by(Subject, DayBlock, StabilityContext) %>% summarySE(measurevar = "RT", groupvars = c("Subject", "DayBlock", "StabilityContext"), na.rm=TRUE)
-#acc_overtime <- summarySE(D, measurevar = "OptimalChoice", groupvars = c("Subject", "DayBlock", "StabilityContext"), na.rm=TRUE)
-
-AccuracyLines <- ggplot(AccDayBlock, aes(x=DayBlock, y=OptimalChoice, Group=Subject)) + #scale_y_continuous(limits = c(0.05, 0.2))+
-  geom_line(aes(group=Subject, colour=Subject), size = 0.1, alpha=.5) +
-  #geom_errorbar(aes(ymin=OptimalChoice-se, ymax=OptimalChoice+se, group=StabilityContext, color=Subject), width=0, size =1) +
-  #geom_point(aes(shape = Congruency, fill=Congruency, group=Congruency), stroke=1, position=position_dodge(.5), color = "Black", size = 4) +
-  facet_grid(StabilityContext~.)+
-  xlab("Overtraining") + ylab("Accuracy")+ggtitle("Accuracy over time")+
-  theme_ERDL_simple()
-AccuracyLines
